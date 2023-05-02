638e45546dc6553c497d5b4f6e71a843ae6e7bf7
francescogentile25 <francescogentile250893@gmail.com>
2023-05-02 16:19:34 +0200

    FEAT add call with axios

---
+++ src/components/Projects.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <p v-for="project in projects" :key="project.id">{{ project.title }}</p>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            projects: []
+        }
+    },
+    methods: {
+        fetchProjects() {
+            axios.get('http://127.0.0.1:8000/api/projects')
+                .then(res => {
+                    console.log(res)
+                })
+        }
+    },
+    mounted() {
+        this.fetchProjects()
+    },
+}
+</script>
+
+<style lang="scss" scoped></style>
